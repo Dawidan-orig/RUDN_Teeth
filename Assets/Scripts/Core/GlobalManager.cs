e65367d42df8f85183be8aebe16274584342a815
Dawidan <andrey9davy@gmail.com>
2024-04-30 21:50:02 +0300

    Feat (UI) : Exit Button
    - Кнопка выхода из приложения
    - И функционал за ней

---
+++ Assets/Scripts/Core/GlobalManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalManager : MonoBehaviour
+{
+    public void ExitApplication() 
+    {
+        Application.Quit();
+    }
+}
